c9ad1cb1a91e0bac1fd089e724a8aa98d788266d
abhishek tripathi <abhishekst.2021@gmail.com>
2020-04-06 02:58:04 +0530

    these are the stirg files

--- strprog.cpp
+++ strprog.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 #include<string> // this is a string header file for string class in cPP but <string.h> is of c programming
 //#include<sstring> // this is a string stream header file
-// string is a secuence of characters
-// last charcter is always null charcter
-// use string class for the safty of the data
-// use charcter array for the speed of the program
-// string class is onr of the part of stl libraries
+// string is a sequence of characters
+// last character is always null character
+// use string class for the safety of the data
+// use character array for the speed of the program
+// string class is one of the part of stl libraries
 using namespace std;
 int main(){
 cout<< "This is string and string stream program in cpp"<<"\n";
-// to calculate the length of the string we use variable name.size() funciton
+// to calculate the length of the string we use variable name.size() function
 // this is a string program we can also use stream string
 // this is a program to simple write the code for string for which we use cin and cout from the std lib
 
@@ -24,19 +24,19 @@ cout << "enter your name"<<endl;
 string s1;
 cin>>s1;
 cout<<s1;
-// here in the above code only the first name comes in the s1 object of the string class bcoz the space is considered as exit
+// here in the above code only the first name comes in the s1 object of the string class because the space is considered as exit
 /*================================
 Various functions in string class are :-
 1. assign() - to assign a value to the string object
-2. append() - to apped the string to the given string
+2. append() - to append the string to the given string
 3. insert() - to insert the characters or string in a string or array of characters
-4. compare() - compare the index according to the ascii code, there can be three types of result those are (0 - same,-1- in dictionary order,1 - not in dictionary order)
+4. compare() - compare the index according to the ASCII code, there can be three types of result those are (0 - same,-1- in dictionary order,1 - not in dictionary order)
 5. replace() - replaces the characters at the index given
 6. erase() - to erase the data in the object
-7. size() - to find the lenght of the array or the string
-8. find() - to find the characters or string in a string, it returns the index number of the first occurance , if its -1 then its not present
+7. size() - to find the length of the array or the string
+8. find() - to find the characters or string in a string, it returns the index number of the first occurrence , if its -1 then its not present
 9. c_str() - converts to c type of string that is char array
-10. rfind() - to find the string from the reverse of the string that is from the last index
+10.rfind() - to find the string from the reverse of the string that is from the last index
 ==================================
 */
 
--- strstream.cpp
+++ strstream.cpp
@@ -1,18 +1,38 @@
 # include <iostream>
-# include <sstream> /* this is the sstream header file included */
+# include <sstream>// above is the header file to include string stream in the program
+#include<String.h>
 #include<stdio.h>
 using namespace std;
+
 int main(){
-/* sstream is used to interchange data types for eg, from string to integer and vice versa. */
-cout<< "This is the program on sstream";
-stringstream strinput; // this is object of the sstream .
-cout <<" enter any string"<<endl;
-string s; // this is the variable for storing string that is to be entered
-cin>>s;
-strinput << s; // here we are writing the value of string variable in the sstream object
-cout<< s+"2"<<endl;
-int i;
-strinput >> i; /* this is the reading from the object of the stringstream and converting string to integer*/
-cout<<"this is the interger number"<< endl<<i+2<<endl;
+cout<<"This is the program for string stream in c++ programs"<<endl;
+/*
+1) the string stream is used to change the string in any data type and other data types like int to string
+2) they are basically the stream
+3) we use >> to read from the string stream object
+4) we use << to write the value in the string stream
+5) we create the object of the string stream to use the functions of the string stream
+6) we use the inbuilt function clear() to clear the value in the string stream
+7) we use "stringstream s(str);" - to break the string into various values in the string stream
+*/
+
+cout << "Enter the value of the integer "<<endl;
+int input;
+cin>>input;
+cout <<"here is the int value"<<input<<endl;
+stringstream sso;
+string str;
+sso<<input; // here we have written the value of the integer in the string stream
+sso >> str; // here we have read from the string stream and added the converted value to the string variable str
+cout<<"Integer value is here "<<input+2<<endl;
+cout<<"String value is here" <<str+"4"<<endl;
+
+string str1 = "44";
+stringstream sso1;
+sso1 << str1; // string value given in the string stream
+int input1;
+sso1>>input1; // string stream value is given in the integer variable
+cout<< str1+"2"<< endl;
+cout<< input1+2<< endl;
 return 0 ;
 }
