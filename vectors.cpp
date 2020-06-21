c04ea2f69e9efbdd88e93133f385bc41c22eddec
Abhi2021 <abhittripathi1996@gmail.com>
2020-06-21 16:01:59 +0530

    commit done

--- vectors.cpp
+++ vectors.cpp
@@ -25,6 +25,8 @@ int main() {
  // to print the value of the vector we can use the same as in array for eg: v3[1]; here []is used to access it.
 for(int i : v3) // this is for each form of for loop
  cout << v3[i]<< endl; // printing all the value of the vector in new line as 0.
+  // we can also use below for_each:
+  // for_each(v1.begin(),v1.end(),function_that_to_execute) : for more info see g4g.
 
  std::vector<string>v4(3,"hello"); // this is a vector with string values to store
  for(int j =0; j < 3 ; j++){
